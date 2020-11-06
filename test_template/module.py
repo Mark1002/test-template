3d49336022547c5e47c515e9608ab44a66422332
mark1002 <mark.chang@mile.cloud>
2020-11-06 16:48:56 +0800

    add mock example

--- test_template/module.py
+++ test_template/module.py
@@ -1,7 +1,19 @@
 """Module service."""
+import requests
 
 
 class ModuleClass1:
 
     def add_two_number(self, n1, n2) -> int:
         return n1 + n2
+
+    def multiply_two_number(self, n1, n2) -> int:
+        return n1 * n2
+
+
+class ModuleClass2:
+
+    def get_my_ip(self) -> str:
+        """Get ip."""
+        response = requests.get('http://ipinfo.io/json')
+        return response.json()['ip']
--- tests/test_test_template.py
+++ tests/test_test_template.py
@@ -1,5 +1,9 @@
+"""Test case for test_template."""
 from test_template import __version__
 from test_template.module import ModuleClass1
+from test_template.module import ModuleClass2
+# for mock
+from test_template.module import requests
 
 
 def test_version():
@@ -29,3 +33,32 @@ class TestModuleClass1:
         total = self.obj.add_two_number(n1, n2)
         # then
         assert total == 100
+
+
+class TestModuleClass2:
+    """Test for module class 2."""
+
+    def setup_method(self):
+        print("setup_method called for every method")
+        self.obj = ModuleClass2()
+
+    def teardown_method(self):
+        print("teardown_method called for every method")
+        del self.obj
+
+    def test_get_my_ip(self, monkeypatch):
+        """Mock test demo."""
+        my_ip = '127.0.0.1'
+
+        class MockResponse:
+            """Mock response class."""
+            def __init__(self, json_body):
+                self.json_body = json_body
+
+            def json(self):
+                return self.json_body
+        # mock request's get function
+        monkeypatch.setattr(
+            requests, 'get', lambda x: MockResponse({'ip': my_ip})
+        )
+        assert self.obj.get_my_ip() == my_ip
