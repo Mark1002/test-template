002aa2edffb9e492306e4ca12062b3cd9afde882
mark1002 <mark.chang@mile.cloud>
2020-11-06 15:50:30 +0800

    refactor test_add_two_number_to_100

--- tests/test_test_template.py
+++ tests/test_test_template.py
@@ -7,11 +7,25 @@ def test_version():
 
 
 def test_demo_fixture(db_conn):
+    """Demo for pytest fixture."""
     assert db_conn.port == '5432'
 
 
 class TestModuleClass1:
     """Test for module class 1."""
 
-    def test_add_two_number(obj: ModuleClass1, db_conn):
-        assert 1 == 2
+    def setup_method(self):
+        print("setup_method called for every method")
+        self.obj = ModuleClass1()
+
+    def teardown_method(self):
+        print("teardown_method called for every method")
+        del self.obj
+
+    def test_add_two_number_to_100(self):
+        # given
+        n1, n2 = 32, 68
+        # when
+        total = self.obj.add_two_number(n1, n2)
+        # then
+        assert total == 100
